af177b9984baf9eec92ad66e2390eb45c015b415
fa-monteiro <monteirof3@hotmail.com>
2022-10-12 12:00:59 -0300

    teste 2

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # calculadora-juventus-da-mooca
-Calculadora customizada com cores, escudo e mascote do time de futebol Juventus da Mooca
+Calculadora customizada com cores, escudo e mascote do time de futebol Juventus da Mooca.
+
+### Por que?
+Personalizar a calculadora é uma forma de animar um pouco algumas tarefas repetirivas do dia-a-dia.
+
+### Como?
+A interface aplicação contém imagens que remetem à cultura do time.
+
+### Cultura do time?
+A cor que representa o clube é o vermelho vinho. O emblema é um "J" branco no centro de um círculo vermelho vinho. O mascote é o Moleque Travesso. Localização: Rua Javari, 117.
+
+### Detalhes:
+Feito com HTML, CSS e JavaScript. Hospedagem no github pages.
+
+##### instagram @felipeuuu
---
+++ script.js
@@ -0,0 +1,69 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator_keys');
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        // Fazer algo ?
+        const key = e.target
+const action = key.dataset.action
+
+if (!action) {
+    console.log('number key!')
+}
+
+if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+) {
+    console.log('operator key!')
+}
+
+if (action === 'decimal') {
+    console.log('decimal key!')
+}
+
+if (action === 'clear') {
+    console.log('clear key!')
+}
+
+if (action === 'calculate') {
+    console.log('equal key!')
+}
+
+    }
+})
+
+const display = document.querySelector('.calculator_display')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+
+        // ta certo issaqui?
+       
+
+    }
+})
+
+if (!action) {
+    if (displayedNum === "0") {
+        display.textContent = keyContent
+    }
+}
+
+if (!action) {
+    if (displayedNum === '0') {
+        display.textContent = keyContent
+    } else {
+        display.textContent = displayedNum + keyContent
+    }
+}
+
+if (action === 'decimal') {
+    display.textContent = displayedNum + '.'
+}
